a50369fe2a58b04c95b7b4cc3fe31be0c88ddea5
linetor <linetor@gmail.com>
2021-11-11 23:21:40 +0900

    [TEST-001] airflow recording test

---
+++ ebs_power_english_recording.py
@@ -0,0 +1,79 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+from configparser import ConfigParser
+import pendulum
+
+import datetime
+date = datetime.datetime.now()
+date_str = date.strftime('%Y-%m-%d')
+
+
+local_tz = pendulum.timezone("Asia/Seoul")
+#args = {'owner': 'linetor', 'start_date': days_ago(n=1)}
+args = {'owner': 'linetor', 'start_date': datetime.datetime(2020, 11, 10, tzinfo=local_tz)}
+
+dag  = DAG(dag_id='ebs_radio_recording',
+           default_args=args,
+           schedule_interval="40 07 * * *")
+
+configparser = ConfigParser()
+radio_address = configparser.get('ebs_address', 'ebs_fm')
+recording_loc = configparser.get('recording_loc', 'recording_loc')
+record_mins = "20"
+
+program_name = "POWER_ENGLISH"
+ori_file = recording_loc + date_str + '_' + ''
+m4a_file = recording_loc + date_str + '_' + 'POWER_ENGLISH' + '.m4a'
+
+recoring_command = f"rtmpdump -r {radio_address} -B {record_mins} -o {ori_file}"
+
+
+recording_task = BashOperator(task_id='recording',
+                  bash_command='recoring_command',
+                  dag=dag)
+
+
+format_command = f"ffmpeg -i {ori_file} -vn -acodec copy  {m4a_file}"
+
+formatting_task = BashOperator(task_id='formatting',
+                  bash_command='format_command',
+                  dag=dag)
+
+
+api_token = configparser.get('dropbox', 'api_token')
+upload_loc = configparser.get('dropbox', 'upload_loc')
+move_loc = configparser.get('dropbox', 'move_loc')
+
+import dropbox
+dbx = dropbox.Dropbox(api_token)
+
+from ebs_radio_cron import recoding_by_shell
+doing_dropbox = PythonOperator(
+    task_id = 'doing_dropbox',
+    python_callable = recoding_by_shell.upload_to_dropbox,
+    op_kwargs={
+        'dbx':dbx ,
+        'upload_loc':upload_loc ,
+        'date_str':date_str ,
+        'move_loc': move_loc,
+        'program_name': program_name
+    },
+    dag = dag
+)
+
+from airflow.operators.dummy_operator import DummyOperator
+start_task = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+end_task = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+start_task >> recording_task >> formatting_task >> doing_dropbox >> end_task
+
+
+
