859fc74d4f854e913d3ef704689f249958889705
linetor <linetor@gmail.com>
2024-03-31 20:29:57 +0900

    server side fix

--- ebs_recording.py
+++ ebs_recording.py
@@ -1,13 +1,15 @@
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.models.variable import Variable
 from datetime import datetime, timedelta
 import pendulum
 import requests
 import os
 
+vault_addr = Variable.get("VAULT_ADDR")
+vault_token = Variable.get("VAULT_TOKEN")
+
 def get_vault_configuration(endpoint):
-    vault_addr = os.environ.get("VAULT_ADDR")
-    vault_token = os.environ.get("VAULT_TOKEN")
     endpoint = f"{vault_addr}/v1/kv/data/{endpoint}"
 
     # HTTP GET 요청을 통해 데이터를 가져옵니다.
@@ -33,8 +35,8 @@ default_args = {
     'start_date': datetime(2024, 3, 19, tzinfo=local_tz),
     'email_on_failure': True,
     'email_on_retry': True,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    #'retries': 1,
+    #'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
@@ -42,14 +44,20 @@ dag = DAG(
     default_args=default_args,
     catchup=False,
     description='Run ebs_recording Python script on rasp 3 server',
-    schedule_interval='20 06 * * 1-6 | 00 06 * * 1-6 | 40 06 * * 1-6 | 40 07 * * 1-6',
+    schedule_interval='20 06 * * 1-6 ',
 )
 
-
+import pytz
+current_time = datetime.now(pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%d_%H:%M')
 
 run_script_task = SSHOperator(
     task_id='run_script',
-    ssh_conn_id='ssh_rasp3',  # Airflow Connection에서 설정한 SSH 연결 ID 입력
-    command=f'{python_loc} {script_loc}',  # 실행할 Python 스크립트 경로 입력
+    ssh_conn_id='ssh_rasp4',  # Airflow Connection에서 설정한 SSH 연결 ID 입력
+    command=f'{python_loc} {script_loc} --start_time_str="{current_time}"',  # 실행할 Python 스크립트 경로 입력
+    environment={
+        'VAULT_ADDR': vault_addr,
+        'VAULT_TOKEN' : vault_token,
+    },
+    cmd_timeout=1800,     
     dag=dag,
 )
