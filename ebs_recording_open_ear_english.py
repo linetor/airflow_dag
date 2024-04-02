b6a37b20050398717f13bbfe180b2b77e7c2100c
linetor <linetor@gmail.com>
2024-04-02 21:35:32 +0900

    fix dag name

--- ebs_recording_open_ear_english.py
+++ ebs_recording_open_ear_english.py
@@ -44,7 +44,7 @@ default_args = {
 #07:40 Power English
 
 dag = DAG(
-    'run_ebs_recording_scrip',
+    'run_ebs_recording_scrip for open ear',
     default_args=default_args,
     catchup=False,
     description='Run ebs_recording Python script on rasp 3 server',
